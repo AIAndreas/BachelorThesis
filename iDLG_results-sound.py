a1f0b5aed3f116289a0b538b23cef976ee68ec0a
LivDreyer <liv.dreyer.johansen@gmail.com>
2024-10-21 11:35:22 +0200

    changes

--- iDLG_results-sound.py
+++ iDLG_results-sound.py
@@ -116,7 +116,6 @@ if num_exp == len(mse_iDLG):
     # Iterate through each mse value
     for i in range(len(mse_iDLG)):
 
-
         if mse_iDLG[i] > 0.01 and mse_iDLG[i] <=11 :
             above_thres_values_small.append(mse_iDLG[i])
         if mse_iDLG[i] > 11:
@@ -152,6 +151,9 @@ if num_exp == len(mse_iDLG):
 # Now, fidelity_count_DLG and fidelity_count_iDLG contain the fidelity for each threshold
 
 print(fidelity_count_iDLG)
+
+print("Number of values (MSE) larger than 0.01 but smaller than 11:", len(above_thres_values_small))
+print("Number of values (MSE) larger than 11:", len(above_thres_values_large), ". Largest number:", max(above_thres_values_large))
 
 
 print("Number of values (MSE) larger than 0.01 but smaller than 11:", len(above_thres_values_small))
